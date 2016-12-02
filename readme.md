024bcda1e6f949117319dd5fcb60c87c9b4a3005
wssgcg1213 <i@zeroling.com>
2016-12-02 16:18:42 +0800

    Initialize repository

---
+++ readme.md
@@ -0,0 +1,24 @@
+# redui-component example
+
+## 目录约定
+
+- readme.md `required`
+- package.json `required`
+- redfile.json `required` # 配置
+- src
+  - index.js `required`# 入口文件
+
+## redfile.json
+
+```json
+{
+  "type": "component",
+  "options": {}
+}
+```
+
+
+
+
+
+Ling.
---
+++ src/Component.jsx
@@ -0,0 +1,28 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class ComponentExample extends Component {
+  static displayName = "ComponentExample"
+
+  static propTypes = {
+    foo: PropTypes.string
+  }
+
+  static defaultProps = {
+
+  }
+
+  state = {}
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    const { children, ...others } = this.props
+    return (
+      <div className="component-example" {...others} >
+        {children}
+      </div>
+    )
+  }
+}
